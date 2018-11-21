faa0c6226eed9de3b0f426d66d18e38e4ef3a6d8
yanyaming <yanyaming4721@qq.com>
2018-11-21 23:35:25 +0800

    2018.11.21 fix git bug4

---
+++ mysite/modules/imageProcess/imageProcess.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+import os
+import click
+from io import BytesIO
+from PIL import Image,ImageFilter, ImageOps
+
+
+'''
+PIL只处理位图
+
+FORMAT文件格式:
+    JPEG:有损压缩RGB位图
+    PNG:无损压缩RGBA位图
+    BMP:原始RGB位图
+    GIF:无损压缩CMYK位图可动态
+
+MODE像素通道模式:
+    1：1位像素，表示黑和白，但是存储的时候每个像素存储为8bit。
+    L：8位像素，表示黑和白。
+    P：8位像素，使用调色板映射到其他模式。
+    RGB：3x8位像素，为真彩色。
+    RGBA：4x8位像素，有透明通道的真彩色。
+    CMYK：4x8位像素，颜色分离。
+    YCbCr：3x8位像素，彩色视频格式。
+    I：32位整型像素。
+    F：32位浮点型像素。
+    PIL也支持一些特殊的模式，包括RGBX（有padding的真彩色）和RGBa（有自左乘alpha的真彩色）。
+
+SIZE尺寸:
+    直角坐标，原点在左上角为(0,0)，每个像素（px=pixel）的宽高为1x1，左上角的像素的中心点是(0.5,0.5)
+    尺寸值：(x-width,y-height)
+    位置坐标：(x-pos,y-pos)
+
+BANDS通道:
+    每个像素点的数据单元，比如png有四通道(R,G,B,A)
+
+'''
+
+
+class ImageProcess:
+    def __init__(self,image_path1):
+        self.im1 = Image.open(image_path1)
+        self.imo = self.im1
+
+    @staticmethod
+    def func_liangdu_jia(c):
+        return c*1.3
+
+    @staticmethod
+    def func_liangdu_jian(c):
+        return c*0.7
+
+    def info(self,image_path2):
+        info_str1 = 'format:'+str(self.im1.format)+\
+                   '\nmode:'+str(self.im1.mode)+\
+                   '\nsize:'+str(self.im1.size)+ \
+                   '\nbands:' + str(self.im1.getbands())
+        info_str2 = ''
+        if image_path2:
+            im2 = Image.open(image_path2)
+            info_str2 += 'format:' + str(im2.format) + \
+                       '\nmode:' + str(im2.mode) + \
+                       '\nsize:' + str(im2.size)+ \
+                       '\nbands:' + str(im2.getbands())
+        return info_str1,info_str2
+
+    def blend(self,image_path2,blend_alpha):
+        if image_path2:
+            im2 = Image.open(image_path2)
+            if im2.size != self.imo.size:
+                im2c = im2.resize(self.imo.size)
+            else:
+                im2c = im2
+            self.imo = Image.blend(self.imo,im2c,blend_alpha)  # out = image1 *(1.0 - alpha) + image2 * alpha
+
+    def composite(self,image_path2,composite_path):
+        if image_path2 and composite_path:
+            im2 = Image.open(image_path2)
+            if im2.size != self.imo.size:
+                im2c = im2.resize(self.imo.size)
+            else:
+                im2c = im2
+            mask = Image.open(composite_path)
+            if mask.size != self.imo.size:
+                maskc = mask.resize(self.imo.size)
+            else:
+                maskc = mask
+            self.imo = Image.composite(self.imo,im2c,maskc)  # out = image1 + image2 * mask[alpha]
+
+    def eval(self,eval_liangdu=0,eval_fanse=False,eval_heibai=False,eval_erzhi=False):
+        if eval_liangdu == 1:
+            self.imo = Image.eval(self.imo,self.func_liangdu_jia)
+        if eval_liangdu == -1:
+            self.imo = Image.eval(self.imo,self.func_liangdu_jian)
+        if eval_liangdu == 0:
+            pass
+        if eval_fanse:
+            self.imo = ImageOps.invert(self.imo)
+        if eval_heibai:
+            self.imo = self.imo.convert('L')
+        if eval_erzhi:
+            self.imo = self.imo.convert('1')
+
+    def filter(self,filter_blur=False,filter_contour=False):
+        if filter_blur:
+            self.imo = self.imo.filter(ImageFilter.GaussianBlur)
+        if filter_contour:
+            self.imo = self.imo.filter(ImageFilter.CONTOUR)
+
+    def convert_image(self,image_path2='',info=False,blend=False,composite=False,eval=False,filter=False,
+                      blend_alpha=0.5,
+                      composite_path='',
+                      eval_liangdu=0,eval_fanse=False,eval_heibai=False,eval_erzhi=False,
+                      filter_blur=False,filter_contour=False):
+        if info:
+            return self.info(image_path2)
+        if blend:
+            self.blend(image_path2,blend_alpha)
+        if composite:
+            self.composite(image_path2,composite_path)
+        if eval:
+            self.eval(eval_liangdu,eval_fanse,eval_heibai,eval_erzhi)
+        if filter:
+            self.filter(filter_blur,filter_contour)
+        image_data = BytesIO()  # 将图片数据暂存到内存中
+        self.imo.save(image_data, 'jpeg')
+        return image_data.getvalue()
+
+    def convert_image_cli(self,output_path,image_path2,op,
+                        blend_alpha=0.3,
+                        composite_path='',
+                        eval_liangdu=0,eval_fanse=False,eval_heibai=True,eval_erzhi=False,
+                        filter_blur=False,filter_contour=False):
+        if op == 'info':
+            imo = self.info(image_path2)
+            print(imo)
+        elif op == 'blend':
+            self.blend(image_path2,blend_alpha)
+        elif op == 'composite':
+            self.composite(image_path2,composite_path)
+        elif op == 'eval':
+            self.eval(eval_liangdu, eval_fanse, eval_heibai, eval_erzhi)
+        elif op == 'filter':
+            self.filter(filter_blur,filter_contour)
+        self.imo.save(output_path, 'jpeg')
+
+
+if __name__ == '__main__':
+    #命令行参数工具click
+    @click.command()
+    @click.option("-p", default='info', type=click.Choice(['info','blend', 'composite','filter','eval']), help=u'操作选项')
+    # @click.option("-e", is_flag=True, help=u'二值化')  # is_flag以参数名控制，不需要写参数值
+    @click.option("-o",default='', help=u'输出文件名')
+    @click.argument("filename1")
+    @click.argument("filename2",default='')
+    def convert(filename1,filename2,o,p):
+        converter = ImageProcess(filename1)
+        if o:
+            converter.convert_image_cli(os.path.join('output',o),filename2,p)
+        else:
+            converter.convert_image_cli(os.path.join('output',filename1),filename2, p)
+    convert()
---
+++ mysite/modules/video2chars/video2chars.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import os
+from pkg_resources import resource_stream
+import numpy
+from moviepy.editor import *
+from PIL import Image, ImageFont, ImageDraw
+import click
+#库的缺陷，没办法用绝对路径，只能把字体放到当前路径
+# font_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))),'static/font/')
+
+
+"""参考原作者代码https://github.com/ryan4yin"""
+"""视频转码过程非常耗CPU资源，只能线下提前转好，线上服务器不可使用"""
+
+class Video2Chars:
+    def __init__(self, video_path, fps, pixels, chars_width, t_start=0, t_end=None):
+        """
+        :param video_path: 字符串, 视频文件的路径
+        :param fps: 生成的视频的帧率
+        :param pixels:用于充当像素的字符
+        :param chars_width: 字符的宽度（以像素记），会影响最终视频的分辨率。
+        :param t_start, t_end: 视频的开始时间和结束时间，只处理指定时间段的视频。
+        """
+        # 加载视频,并截取
+        video_clip = VideoFileClip(video_path).subclip(t_start, t_end)
+        self.fps = fps
+        #像素形状（按照视觉由深到浅排序，后面用于灰度值）
+        self.pixels = pixels if pixels else \
+            "MNEFTmneotli\"'. " #16阶
+        self.chars_width = chars_width
+        self.chars_height = int(chars_width/video_clip.aspect_ratio)#宽高比
+        self.video_clip:VideoClip = video_clip.resize((self.chars_width, self.chars_height))
+        # 字体相关
+        font_fp = resource_stream("video2chars", "DroidSansMono.ttf")
+        self.font = ImageFont.truetype(font_fp, size=14)  # 使用等宽字体
+        self.font_width = sum(self.font.getsize("a")) // 2  # 为了保证像素宽高一致，均取宽高的平均值
+        # 产生的视频的宽高（以像素记）
+        self.video_size = int(self.chars_width * self.font_width), int(self.chars_height * self.font_width)
+
+    def get_char_by_gray(self, gray):
+        """通过灰度值从 pixels 中挑选字符，充当字符动画中的‘像素’"""
+        percent = gray / 255  # 转换到 0-1 之间
+        index = int(percent * (len(self.pixels) - 1))  # 拿到index“颜色值”
+        return self.pixels[index]
+
+    def get_chars_frame(self, t):
+        """将每一帧图片转换为字符画"""
+        # 获取到图像
+        img_np = self.video_clip.get_frame(t)
+        img = Image.fromarray(img_np, "RGB")
+        img_gray = img.convert(mode="L")
+        # 新建画布
+        img_chars = Image.new("RGB", self.video_size, color="white")
+        brush = ImageDraw.Draw(img_chars)  # 画笔
+        for y in range(self.chars_height):
+            for x in range(self.chars_width):
+                r, g, b = img_np[y][x]
+                gray = img_gray.getpixel((x, y))
+                char = self.get_char_by_gray(gray)
+                position = x * self.font_width, y * self.font_width  # x 横坐标（宽），y纵坐标（高，而且向下为正）
+                brush.text(position, char, fill=(r, g, b), font=self.font)
+        return numpy.array(img_chars)
+
+    def generate_chars_video(self):
+        """生成字符视频对象"""
+        clip = VideoClip(self.get_chars_frame, duration=self.video_clip.duration)
+        return clip.set_fps(self.fps).set_audio(self.video_clip.audio)
+
+
+if __name__ == '__main__':
+    #命令行参数工具click
+    @click.command()
+    @click.option("--chars_width", default=150, help='The width of the generated video, in characters, default to 200')
+    @click.option("--fps", default=8, help='frames per second, defaults to 8')
+    @click.option("--pixels", default=None, type=str, help='the chars sequence used to generate character animation')
+    @click.option("--t_start", default=0, help="the start time that the video needs to be converted(in seconds)")
+    @click.option("--t_end", default=None, type=int, help="the end time that the video needs to be converted(in seconds)")
+    @click.option("--output", default="output.mp4", help='output to a file with this name, default to "output.mp4"')
+    @click.argument("filename")
+    def convert(filename, chars_width, fps, pixels, output, t_start, t_end):
+        converter = Video2Chars(video_path=filename,
+                                fps=fps,
+                                chars_width=chars_width,
+                                t_start=t_start,
+                                t_end=t_end,
+                                pixels=pixels)
+
+        clip = converter.generate_chars_video()
+        clip.write_videofile(os.path.join('output',filename))
+    convert()
